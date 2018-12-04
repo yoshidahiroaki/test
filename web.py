f834656fcf6c8975309c194148d3f505bcef54b5
yoshidahiroaki <34274426+yoshidahiroaki@users.noreply.github.com>
2018-12-04 10:12:13 +0900

    Update web.py

--- web.py
+++ web.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
-    return "Hello World!だよ"
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    
